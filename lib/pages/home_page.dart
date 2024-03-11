1a611f949fb10bacd99959393cc67ed75c11b405
khurshid <shamsun.com@gmail.com>
2024-03-11 17:25:53 +0500

    HTTP packaged implemented

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../models/post_model.dart';
+import '../services/http_service.dart';
+import '../services/log_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,7 +12,52 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  String response = "no data";
+  String data = "no data";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadPosts();
+    //createPost();
+    //updatePost();
+    //deletePost();
+  }
+
+  loadPosts() async {
+    var response = await Network.GET(Network.API_POST_LIST, {});
+    LogService.d(response!);
+    setState(() {
+      data = response;
+    });
+  }
+
+  createPost() async {
+    Post post = Post(title: "Nextgen", body: "Academy", userId: 1);
+    var response = await Network.POST(Network.API_POST_CREATE, Network.paramsCreate(post));
+    LogService.d(response!);
+    setState(() {
+      data = response;
+    });
+  }
+
+  updatePost() async {
+    Post post = Post(id: 1, title: "Nextgen 2", body: "Academy 2", userId: 1);
+    var response = await Network.PUT(Network.API_POST_UPDATE + post.id.toString(), Network.paramsUpdate(post));
+    LogService.d(response!);
+    setState(() {
+      data = response;
+    });
+  }
+
+  deletePost() async {
+    Post post = Post(id: 1, title: "Nextgen 2", body: "Academy 2", userId: 1);
+    var response = await Network.DEL(Network.API_POST_DELETE + post.id.toString(), Network.paramsEmpty());
+    LogService.d(response!);
+    setState(() {
+      data = response;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,7 +67,7 @@ class _HomePageState extends State<HomePage> {
         title: Text("Networking"),
       ),
       body: Center(
-        child: Text(response),
+        child: Text(data, style: TextStyle(fontSize: 20),),
       ),
     );
   }
