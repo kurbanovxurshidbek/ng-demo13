1128312280ca36fe7cccba826369846e73a5323b
khurshid <shamsun.com@gmail.com>
2024-03-13 17:26:34 +0500

    Parsing and UI implemented

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:ngdemo13/pages/add_post_page.dart';
 import '../models/post_model.dart';
 import '../services/http_service.dart';
 import '../services/log_service.dart';
@@ -13,37 +14,25 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   String data = "no data";
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    loadPosts();
-    //createPost();
-    //updatePost();
-    //deletePost();
-  }
+  bool isLoading = true;
+  List<Post> posts = [];
 
   loadPosts() async {
-    var response = await Network.GET(Network.API_POST_LIST, {});
+    var response =
+        await Network.GET(Network.API_POST_LIST, Network.paramsEmpty());
     LogService.d(response!);
+    List<Post> postList = Network.parsePostList(response!);
     setState(() {
-      data = response;
-    });
-  }
-
-  createPost() async {
-    Post post = Post(title: "Nextgen", body: "Academy", userId: 1);
-    var response = await Network.POST(Network.API_POST_CREATE, Network.paramsCreate(post));
-    LogService.d(response!);
-    setState(() {
-      data = response;
+      posts = postList;
+      isLoading = false;
     });
   }
 
   updatePost() async {
     Post post = Post(id: 1, title: "Nextgen 2", body: "Academy 2", userId: 1);
-    var response = await Network.PUT(Network.API_POST_UPDATE + post.id.toString(), Network.paramsUpdate(post));
+    var response = await Network.PUT(
+        Network.API_POST_UPDATE + post.id.toString(),
+        Network.paramsUpdate(post));
     LogService.d(response!);
     setState(() {
       data = response;
@@ -52,22 +41,79 @@ class _HomePageState extends State<HomePage> {
 
   deletePost() async {
     Post post = Post(id: 1, title: "Nextgen 2", body: "Academy 2", userId: 1);
-    var response = await Network.DEL(Network.API_POST_DELETE + post.id.toString(), Network.paramsEmpty());
+    var response = await Network.DEL(
+        Network.API_POST_DELETE + post.id.toString(), Network.paramsEmpty());
     LogService.d(response!);
     setState(() {
       data = response;
     });
   }
 
+  Future _callAddPostPage() async {
+    bool result = await Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) {
+      return const AddPostPage();
+    }));
+
+    if (result) {
+      loadPosts();
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadPosts();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: Text("Networking"),
+        ),
+        body: Stack(
+          children: [
+            ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: (ctx, index) {
+                return _itemOfPost(posts[index]);
+              },
+            ),
+            isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : SizedBox.shrink(),
+          ],
+        ),
+      floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
-        title: Text("Networking"),
+        child: Icon(Icons.add,color: Colors.white,),
+        onPressed: (){
+          _callAddPostPage();
+        },
       ),
-      body: Center(
-        child: Text(data, style: TextStyle(fontSize: 20),),
+    );
+  }
+
+  Widget _itemOfPost(Post post) {
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            post.title!,
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          Text(post.body!,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+          Divider(),
+        ],
       ),
     );
   }
